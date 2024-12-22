318c39a7ae84c53bde87ef76d5d883330b4bc0f9
Kseniia Korniichuk <kornksen010784@gmail.com>
2024-12-23 00:14:59 +0200

    add js and correction html

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,6 +1,17 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
+document.addEventListener('DOMContentLoaded', () => {
+ 
+  const accordion = new Accordion('.faq-wrap', {
+    duration: 300, 
+    showMultiple: false, 
+  });
+
+  console.log('Accordion initialized:', accordion);
+});
+
+
 
 
 
