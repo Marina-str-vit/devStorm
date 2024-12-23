0dc53228f9ea725ed71b6cfcf81e04c2ac7bc2aa
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2024-12-23 22:58:21 +0100

    Add first markup of third part

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,8 +1,21 @@
 const aboutMeSection = document.querySelector('.accordion-container');
+const swiperSection = document.querySelector('.swiper')
 
+/* For part 2 */
 import Accordion from 'accordion-js';
+
+/* For part 3 */
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 
 new Accordion(aboutMeSection, {
     openOnInit: [0],
     collapse: true
 })
+
+const swiper = new Swiper(swiperSection, {
+    modules: [Navigation]
+});
