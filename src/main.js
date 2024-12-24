5e505f483cae822b79f1b1a589f8361f37dcb366
Marina Strukova <meribsv@gmail.com>
2024-12-24 12:22:05 +0200

    update main.js

--- src/main.js
+++ src/main.js
@@ -10,8 +10,9 @@ import 'izitoast/dist/css/iziToast.min.css';
 // import './js/header.js';
 import './js/about-me.js';
 import './js/projects.js';
+import './js/faq.js';
 // import './js/covers.js';
 // import './js/reviews.js';
 // import './js/footer.js';
 // import './js/modal.js';
-// import './js/faq.js';
+
