745f880906fde5bb3d454413509a8a4c116585a0
Yuliia Shvets <y.shvets85@gmail.com>
2024-12-25 16:30:06 +0100

    add covers.js

---
+++ src/js/covers.js
@@ -0,0 +1,9 @@
+const refs = {  coversWrapperListItem: document.querySelectorAll('.covers-wrapper-list-item'),
+    coversContent: document.querySelector('.covers-content'),};
+  const observer = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {    for (let i = 0; i < refs.coversWrapperListItem.length; i += 1) {
+        refs.coversWrapperListItem[i].classList.add('covers-animation');    }
+    } else {    for (let i = 0; i < refs.coversWrapperListItem.length; i += 1) {
+        refs.coversWrapperListItem[i].classList.remove('covers-animation');    }
+    }});
+  observer.observe(refs.coversContent);
