c1a3e98b7abf7cd4ce8ff10e54fa6f18c1828e6c
Marina Strukova <meribsv@gmail.com>
2024-12-27 16:16:40 +0200

    Update projects.js

--- src/js/projects.js
+++ src/js/projects.js
@@ -15,13 +15,13 @@ const swiper = new Swiper('.projects-block-slider.swiper-container', {
     on: {
         reachEnd: function () {
             const nextIcon = document.querySelector('.projects-button-slider-next.swiper-button-next svg use');
-            nextIcon.setAttribute('href', `${sprite}#arrow-right`);
+            // nextIcon.setAttribute('href', `${sprite}#arrow-right`);
             const nextButton = document.querySelector('.projects-button-slider-next');
             nextButton.style.borderColor = 'grey';
         },
         reachBeginning: function () {
-            const prevIcon = document.querySelector('.projects-button-slider-prev.swiper-button-prev svg use');
-            prevIcon.setAttribute('href', `${sprite}#arrow-right`);
+            const prevIcon = document.querySelector('.projects-button-slider-prev .swiper-button-prev svg use');
+            // prevIcon.setAttribute('href', `${sprite}#arrow-right`);
             const prevButton = document.querySelector('.projects-button-slider-prev');
             prevButton.style.borderColor = 'grey';
         },
@@ -29,8 +29,8 @@ const swiper = new Swiper('.projects-block-slider.swiper-container', {
             if (!swiper.isBeginning && !swiper.isEnd) {
                 const nextIcon = document.querySelector('.projects-button-slider-next.swiper-button-next svg use');
                 const prevIcon = document.querySelector('.projects-button-slider-prev.swiper-button-prev svg use');
-                nextIcon.setAttribute('href', `${sprite}#arrow-right`);
-                prevIcon.setAttribute('href', `${sprite}#arrow-right`);
+                // nextIcon.setAttribute('href', `${sprite}#arrow-right`);
+                // prevIcon.setAttribute('href', `${sprite}#arrow-right`);
                 
                 const prevButton = document.querySelector('.projects-button-slider-prev');
                 const nextButton = document.querySelector('.projects-button-slider-next');
