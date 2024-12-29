2007b7464ddcd93e83f1886b4e59c297233d8e13
Marina Strukova <meribsv@gmail.com>
2024-12-29 23:06:29 +0200

    js update

--- src/js/projects.js
+++ src/js/projects.js
@@ -14,21 +14,21 @@ const swiper = new Swiper('.projects-block-slider.swiper-container', {
     },
     on: {
         reachEnd: function () {
-            const nextIcon = document.querySelector('.projects-button-slider-next.swiper-button-next svg use');
+            // const nextIcon = document.querySelector('.projects-button-slider-next.swiper-button-next svg use');
             // nextIcon.setAttribute('href', `${sprite}#arrow-right`);
             const nextButton = document.querySelector('.projects-button-slider-next');
             nextButton.style.borderColor = 'grey';
         },
         reachBeginning: function () {
-            const prevIcon = document.querySelector('.projects-button-slider-prev .swiper-button-prev svg use');
+            // const prevIcon = document.querySelector('.projects-button-slider-prev .swiper-button-prev svg use');
             // prevIcon.setAttribute('href', `${sprite}#arrow-right`);
             const prevButton = document.querySelector('.projects-button-slider-prev');
             prevButton.style.borderColor = 'grey';
         },
         slideChange: function() {
             if (!swiper.isBeginning && !swiper.isEnd) {
-                const nextIcon = document.querySelector('.projects-button-slider-next.swiper-button-next svg use');
-                const prevIcon = document.querySelector('.projects-button-slider-prev.swiper-button-prev svg use');
+                // const nextIcon = document.querySelector('.projects-button-slider-next.swiper-button-next svg use');
+                // const prevIcon = document.querySelector('.projects-button-slider-prev.swiper-button-prev svg use');
                 // nextIcon.setAttribute('href', `${sprite}#arrow-right`);
                 // prevIcon.setAttribute('href', `${sprite}#arrow-right`);
                 
